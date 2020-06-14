3467f03b1da5f9199bf4608386ca42fe5aef8b9c
Uladzislau <vladislav.sergienya@gmail.com>
2020-06-14 16:31:27 +0300

    get rid of unused method

--- src/main/java/com/uladzislau/docker_container_starter/exec/TerminalScriptExecutor.java
+++ src/main/java/com/uladzislau/docker_container_starter/exec/TerminalScriptExecutor.java
@@ -2,15 +2,13 @@ package com.uladzislau.docker_container_starter.exec;
 
 import lombok.SneakyThrows;
 
-import java.io.File;
-
 public class TerminalScriptExecutor {
-
     @SneakyThrows
     public static Process executeCommand(String command) {
         ProcessBuilder processBuilder = new ProcessBuilder();
 
-        String[] terminalCommands = prepareWholeCommand(command);
+        String[] terminalCommands = DeveloperSystem.getTerminalOSDependArgs(3);
+        terminalCommands[2] = command;
 
         Process start = processBuilder.command(terminalCommands).start();
         start.waitFor();
@@ -18,28 +16,7 @@ public class TerminalScriptExecutor {
         return start;
     }
 
-    @SneakyThrows
-    public static Process executeWithRedirect(String command, File file) {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-
-        String[] terminalCommands = prepareWholeCommand(command);
-
-        Process start = processBuilder.command(terminalCommands)
-                .redirectInput(file)
-                .redirectError(file)
-                .start();
-        start.waitFor();
-
-        return start;
-    }
-
-    private static String[] prepareWholeCommand(String command) {
-        String[] terminalCommands = DeveloperSystem.getTerminalOSDependArgs(3);
-        terminalCommands[2] = command;
-        return terminalCommands;
-    }
-
-    private static class DeveloperSystem {
+    static class DeveloperSystem {
         private static final String OS = System.getProperty("os.name").split("\\s+")[0];
 
         static String[] getTerminalOSDependArgs(int size) {
